78584861350229d1662ee11beab645a491f62b1d
Christian <chris.rosado95@gmail.com>
2024-09-09 19:21:37 -0400

    -Added a status panel to display the sensors data. -Added an indicator  bar for the bottom sheet and resized and reorganized the bottom sheet. -The bottom sheet now automatically fully expands upon launch of the app and when returning to the homescreen.

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import { Text, View, ImageBackground, ScrollView, TouchableOpacity, Image, StatusBar } from 'react-native';
+import { Text, View, ImageBackground, ScrollView, TouchableOpacity, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import BottomSheet from '@gorhom/bottom-sheet';
@@ -7,6 +7,8 @@ import { BlurView } from 'expo-blur';
 import CustomButton from '../components/CustomButton';
 import { router } from 'expo-router';
 import { styles } from './AppStyles';  // Importing the styles from the new file
+import { StatusBar } from 'expo-status-bar'; // Corrected import for Expo
+
 
 export default function App() {
   const ESP32_IP = 'http://192.168.50.35';  // Replace with your ESP32 IP address
@@ -14,22 +16,45 @@ export default function App() {
   const dayName = new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(today);
   const monthAndDay = new Intl.DateTimeFormat('en-US', { month: 'long', day: 'numeric' }).format(today);
   const sheetRef = useRef(null);
-  const snapPoints = ['10%', '40%'];
+  const snapPoints = ['15%', '32%'];
+
+    // Custom handle component with a centered indicator bar
+    const CustomHandle = () => {
+      return (
+        <View className="items-center justify-center top-3">
+          <View className="bg-gray-500 rounded-full w-10 h-1" />
+        </View>
+      );
+    };
 
   return (
+
+    // HomeScreen General Design
     <GestureHandlerRootView style={styles.container}>
       <SafeAreaView className="bg-primary h-full">
+      <StatusBar backgroundColor="#161622" style="dark" />
         <ImageBackground source={require('../assets/images/gradient.png')} className="flex-1 absolute top-0 left-0 right-0 bottom-0" resizeMode="cover"></ImageBackground>
         <ScrollView contentContainerStyle={{ height: '100%' }}>
           <View className="w-full min-[85vh] px-4">
             <Text className="text-2xl font-bold text-white text-center">{dayName}</Text>
             <Text className="text-xl font-semibold text-white text-center pl-2">{monthAndDay}</Text>
             <View className="relative mt-5">
+
+             {/* AQUARIUM'S STATUS PANEL*/}
+             <View className="bg-gray-50/40 w-full h-56 rounded-xl items-center p-16">
+                
+                {/* <Text className="text-white text-lg font-bold text-center">Insert Status Widgets Here</Text> */}
+               
+              </View>
+
+              {/* HISTORY BUTTON*/}
               <CustomButton
                 title="History"
                 handlePress={() => router.push('/history')}
                 containerStyles="w-full mt-7"
               />
+
+              {/* SETTINGS BUTTON */}
               <CustomButton
                 title="Settings"
                 handlePress={() => router.push('/settings')}
@@ -39,18 +64,21 @@ export default function App() {
           </View>
         </ScrollView>
 
+        {/* BOTTOM SHEET Properties */}
         <BottomSheet
           ref={sheetRef}
-          index={0}
+          index={1}
           snapPoints={snapPoints}
-          handleComponent={null}
+          handleComponent={CustomHandle}
           backgroundStyle={styles.bottomSheetBackground}
           handleStyle={styles.bottomSheetHandle}
+      
         >
+    
           <View style={styles.bottomSheetContent}>
-            <BlurView intensity={40} style={styles.blurContainer}>
+            <BlurView intensity={70} style={styles.blurContainer}>
              {/* Row1 */}
-              <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: 30 }}>
+              <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: 20 }}>
                 
                 {/* Feeding Button */}
                 <TouchableOpacity
@@ -65,7 +93,7 @@ export default function App() {
 
                 {/* Camera Button */}
                 <TouchableOpacity
-                  style={{ width: 90, height: 90, borderRadius: 55, marginHorizontal: 30, justifyContent: 'center', alignItems: 'center' }}
+                  style={{ width: 90, height: 90, borderRadius: 55, marginHorizontal: 40, justifyContent: 'center', alignItems: 'center' }}
                   onPress={() => console.log('Camera Button pressed')}
                 >
                   <Image
@@ -86,7 +114,7 @@ export default function App() {
                 </TouchableOpacity>
               </View>
               {/* Row 2 */}
-              <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: 50 }}>
+              <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: 30 }}>
                 
                 {/* Light Button Needs to be fixed*/}
                 <TouchableOpacity
@@ -101,7 +129,7 @@ export default function App() {
 
                 {/* Calendar Button */}
                 <TouchableOpacity
-                  style={{ width: 90, height: 90, borderRadius: 55, marginHorizontal: 30, justifyContent: 'center', alignItems: 'center' }}
+                  style={{ width: 90, height: 90, borderRadius: 55, marginHorizontal: 40, justifyContent: 'center', alignItems: 'center' }}
                   onPress={() => console.log('Calendar Button pressed')}
                 >
                   <Image
@@ -124,7 +152,6 @@ export default function App() {
             </BlurView>
           </View>
         </BottomSheet>
-        <StatusBar backgroundColor="#161622" style="light" />
       </SafeAreaView>
     </GestureHandlerRootView>
   );
